6101b96f171f38fe8586c359a8af8ecce5c433d6
ajkdrag <altamashjahangirkhan123@gmail.com>
2024-05-03 15:16:24 +0530

    Updated lazy installation

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,7 +2,7 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.fn.filereadable(lazypath) then
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
